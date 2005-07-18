eccac60a5f39b4d7fac449263e94f6287b72ed73
jv <>
2005-07-18 20:00:16 +0000

    Override mutaties.eb with mutaties-fixed.eb, if appropriate.

--- mutaties.sh
+++ mutaties.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-perl -Mlib=$EB_LIB -w $EB_LIB/eximut.pl | tee mutaties.eb |
-perl -Mlib=$EB_LIB -w $EB_LIB/ebshell.pl --echo
+mut=mutaties.eb
+perl -Mlib=$EB_LIB -w $EB_LIB/eximut.pl > $mut
+if [ -s mutaties-fixed.eb ]; then
+    mut=mutaties-fixed.eb
+    echo "Overriding mutaties.eb with $mut" 1>&2
+fi
+perl -Mlib=$EB_LIB -w $EB_LIB/ebshell.pl --echo < $mut 2>&1
