8e4b99fd618a2b2551245a251385558a009cec3c
jv <>
2005-07-29 16:13:48 +0000

    Extend for better detail handling and Proef- en Saldibalans.

--- lib/EB/Report/Text.pm
+++ lib/EB/Report/Text.pm
@@ -1,11 +1,11 @@
 #!/usr/bin/perl -w
-my $RCS_Id = '$Id: Text.pm,v 1.1 2005/07/14 12:54:08 jv Exp $ ';
+my $RCS_Id = '$Id: Text.pm,v 1.2 2005/07/29 16:13:48 jv Exp $ ';
 
 # Author          : Johan Vromans
 # Created On      : Sat Jun 11 13:44:43 2005
 # Last Modified By: Johan Vromans
-# Last Modified On: Sat Jul  9 21:43:50 2005
-# Update Count    : 99
+# Last Modified On: Fri Jul 29 14:51:33 2005
+# Update Count    : 116
 # Status          : Unknown, Use with caution!
 
 ################ Common stuff ################
@@ -18,26 +18,34 @@ use warnings;
 use EB::Globals;
 use EB::Finance;
 
+my $fmt = "%-6s  %-40.40s  %9s  %9s  %9s  %9s\n";
+
 sub new {
-    bless {};
+    my $class = shift;
+    my $self = { @_ };
+
+    $self->{hdr} =
+      sprintf($fmt, "RekNr",
+	      $self->{verdicht} ? "Verdichting/Grootboekrekening" : "Grootboekrekening",
+	      "Debet", "Credit",
+	      $self->{proef} ? ("Saldo Db", "Saldo Cr") : ('', '')
+	     );
+    $self->{hdr} =~ s/ +$//;
+    ($self->{line} = $self->{hdr}) =~ tr/\n/-/c;
+
+    bless $self, $class;
 }
 
-my $fmt = "%-6s  %-40.40s  %9s  %9s\n";
-
 sub addline {
-    my ($self, $type, $acc, $desc, $deb, $crd) = @_;
+    my ($self, $type, $acc, $desc, $deb, $crd, $sdeb, $scrd) = @_;
     if ( $type eq 'H' ) {
 	print($desc, "\n\n") if $desc;
-	my $hdr = sprintf($fmt, "RekNr", "Grootboekrekening", "Debet", "Credit");
-	print($hdr);
-	$hdr =~ tr/\n/-/c;
-	print($hdr);
+	print($self->{hdr});
+	print($self->{line});
 	return;
     }
     if ( $type eq 'T' ) {
-	my $hdr = sprintf($fmt, "RekNr", "Grootboekrekening", "Debet", "Credit");
-	$hdr =~ tr/\n/-/c;
-	print($hdr);
+	print($self->{line});
     }
     if ( $deb && $deb <= 0 && !$crd ) {
 	($deb, $crd) = ('', -$deb);
@@ -45,7 +53,7 @@ sub addline {
     elsif ( $crd && $crd <= 0 && !$deb ) {
 	($deb, $crd) = (-$crd, '');
     }
-    for ( $deb, $crd ) {
+    for ( $deb, $crd, $sdeb, $scrd ) {
 	$_ = $_ ? numfmt($_) : '';
     }
     if ( $type =~ /^D(\d+)/ ) {
@@ -54,7 +62,8 @@ sub addline {
     elsif ( $type =~ /^[HT](\d+)/ ) {
 	$desc = (" " x ($1-1)) . $desc;
     }
-    printf($fmt, $acc, $desc, $deb, $crd);
-# TODO   print("\n") if $detail > 0 && $type =~ /^T\d+$/;
-    print("\n") if $type =~ /^T\d+$/;
+    my $t = sprintf($fmt, $acc, $desc, $deb, $crd, $sdeb, $scrd);
+    $t =~ s/ +$//;
+    print($t);
+    print("\n") if $type =~ /^T(\d+)$/ && $1 <= $self->{detail};
 }
