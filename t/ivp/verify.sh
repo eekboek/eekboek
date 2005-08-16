8175eafa658fb8940dc822bc006af00fd7c8cfd6
jv <>
2005-08-16 20:27:40 +0000

    Remove SQL and logfiles

--- t/ivp/verify.sh
+++ t/ivp/verify.sh
@@ -1,30 +1,36 @@
-csh <<'EOF'
-source setup.csh
-setenv EB_SQL_NOW 2004-12-31
+#!/bin/sh
+
+. ./setup.sh
+
+EB_SQL_NOW=2004-12-31; export EB_SQL_NOW
+
+rm -f *.sql *.log
+
 echo "=== $EB_DB_NAME === newdb ==="
-sh ./newdb.sh >& newdb.log
+rebuild > newdb.log 2>&1
+
 echo "=== $EB_DB_NAME === mutaties ==="
-perl -I$EB_LIB $EB_LIB/ebshell.pl --echo < mutaties.eb >& mutaties.log
+ebshell --echo < mutaties.eb > mutaties.log 2>&1
+
 echo "=== $EB_DB_NAME === verificatie ==="
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c balans | diff -c - balans.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c balans --detail=0 | diff -c - balans0.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c balans --detail=1 | diff -c - balans1.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c balans --detail=2 | diff -c - balans2.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c result | diff -c - result.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c result --detail=0 | diff -c - result0.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c result --detail=1 | diff -c - result1.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c result --detail=2 | diff -c - result2.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c journaal | diff -c - journaal.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c journaal postbank | diff -c - journaal-postbank.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c journaal postbank:24 | diff -c - journaal-postbank24.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c journaal 48 | diff -c - journaal-48.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c proefensaldibalans | diff -c - proef.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c proefensaldibalans --detail=0 | diff -c - proef0.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c proefensaldibalans --detail=1 | diff -c - proef1.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c proefensaldibalans --detail=2 | diff -c - proef2.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c grootboek | diff -c - grootboek.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c grootboek --detail=0 | diff -c - grootboek0.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c grootboek --detail=1 | diff -c - grootboek1.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c grootboek --detail=2 | diff -c - grootboek2.txt
-perl -I$EB_LIB $EB_LIB/ebshell.pl -c btwaangifte j | diff -c - btw.txt
-EOF
+ebshell -c balans | diff -c - balans.txt
+ebshell -c balans --detail=0 | diff -c - balans0.txt
+ebshell -c balans --detail=1 | diff -c - balans1.txt
+ebshell -c balans --detail=2 | diff -c - balans2.txt
+ebshell -c result | diff -c - result.txt
+ebshell -c result --detail=0 | diff -c - result0.txt
+ebshell -c result --detail=1 | diff -c - result1.txt
+ebshell -c result --detail=2 | diff -c - result2.txt
+ebshell -c journaal | diff -c - journaal.txt
+ebshell -c journaal postbank | diff -c - journaal-postbank.txt
+ebshell -c journaal postbank:24 | diff -c - journaal-postbank24.txt
+ebshell -c journaal 48 | diff -c - journaal-48.txt
+ebshell -c proefensaldibalans | diff -c - proef.txt
+ebshell -c proefensaldibalans --detail=0 | diff -c - proef0.txt
+ebshell -c proefensaldibalans --detail=1 | diff -c - proef1.txt
+ebshell -c proefensaldibalans --detail=2 | diff -c - proef2.txt
+ebshell -c grootboek | diff -c - grootboek.txt
+ebshell -c grootboek --detail=0 | diff -c - grootboek0.txt
+ebshell -c grootboek --detail=1 | diff -c - grootboek1.txt
+ebshell -c grootboek --detail=2 | diff -c - grootboek2.txt
+ebshell -c btwaangifte j | diff -c - btw.txt
