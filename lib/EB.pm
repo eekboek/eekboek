9f24fa7717ce034efebadb60a65b35287663ae30
jv <>
2005-09-28 21:02:25 +0000

    Bumb version.

--- lib/EB.pm
+++ lib/EB.pm
@@ -1,10 +1,10 @@
 # EB.pm -- 
-# RCS Info        : $Id: EB.pm,v 1.9 2005/09/27 21:12:04 jv Exp $
+# RCS Info        : $Id: EB.pm,v 1.10 2005/09/28 21:02:25 jv Exp $
 # Author          : Johan Vromans
 # Created On      : Fri Sep 16 18:38:45 2005
 # Last Modified By: Johan Vromans
-# Last Modified On: Tue Sep 27 23:12:00 2005
-# Update Count    : 73
+# Last Modified On: Wed Sep 28 23:02:20 2005
+# Update Count    : 74
 # Status          : Unknown, Use with caution!
 
 our $app;
@@ -15,7 +15,7 @@ use strict;
 use base qw(Exporter);
 
 our $VERSION;
-$VERSION = "0.11";
+$VERSION = "0.12";
 
 our @EXPORT;
 our @EXPORT_OK;
