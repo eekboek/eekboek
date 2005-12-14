d5f6bf58b7eeef9f3135b8e4c3464bebecda7d6b
jv <>
2005-12-14 16:20:31 +0000

    CPAN documentation module.

---
+++ lib/EekBoek.pm
@@ -0,0 +1,58 @@
+package EekBoek;
+
+$VERSION = '0.22';
+
+# NOTE: This is a documentation-only module.
+
+use strict;
+
+=head1 NAME
+
+EekBoek - Bookkeeping software for small and medium-size businesses
+
+=head1 SYNOPSIS
+
+EekBoek is a bookkeeping package for small and medium-size businesses.
+Unlike other accounting software, EekBoek has both a command-line
+interface (CLI) and a graphical user-interface (GUI). Furthermore, it
+has a complete Perl API to create your own custom applications.
+
+EekBoek is designed for the Dutch/European market and currently
+available in Dutch only. An English translation is in the works (help
+appreciated).
+
+=head1 DESCRIPTION
+
+For a description how to use the program, see L<http://www.squirrel.nl/eekboek/docs/index.html>.
+
+=head1 BUGS AND PROBLEMS
+
+Please use the eekboek-users mailing list at SourceForge.
+
+=head1 AUTHOR AND CREDITS
+
+Johan Vromans (jvromans@squirrel.nl) wrote this module.
+
+Web site: L<http://www.squirrel.nl/eekboek>.
+
+=head1 COPYRIGHT AND DISCLAIMER
+
+This program is Copyright 2005 by Squirrel Consultancy. All
+rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of either: a) the GNU General Public License as
+published by the Free Software Foundation; either version 1, or (at
+your option) any later version, or b) the "Artistic License" which
+comes with Perl.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See either the
+GNU General Public License or the Artistic License for more details.
+
+=cut
+
+1;
+
+# $Id: EekBoek.pm,v 1.1 2005/12/14 16:20:31 jv Exp $
