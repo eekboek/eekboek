d177ace3cb98efdbcf3e0e0e3daa0d322188f756
jv <>
2006-01-12 21:16:52 +0000

    Make multiple selection arguments work. Widen Boekstuk column. Rework to handle payments w/o bookings. Handle partial payments.

--- lib/EB/Report/Debcrd.pm
+++ lib/EB/Report/Debcrd.pm
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-my $RCS_Id = '$Id: Debcrd.pm,v 1.6 2006/01/05 17:59:53 jv Exp $ ';
+my $RCS_Id = '$Id: Debcrd.pm,v 1.7 2006/01/12 21:16:52 jv Exp $ ';
 
 package main;
 
@@ -12,8 +12,8 @@ package EB::Report::Debcrd;
 # Author          : Johan Vromans
 # Created On      : Wed Dec 28 16:08:10 2005
 # Last Modified By: Johan Vromans
-# Last Modified On: Thu Jan  5 18:40:47 2006
-# Update Count    : 118
+# Last Modified On: Thu Jan 12 18:44:20 2006
+# Update Count    : 167
 # Status          : Unknown, Use with caution!
 
 ################ Common stuff ################
@@ -46,7 +46,9 @@ sub crediteuren {
 sub _perform {
     my ($self, $args, $opts, $debcrd) = @_;
 
-    $args = uc(join("|", @$args)) if $args;
+    if ( $args ) {
+	$args = join("|", map { quotemeta($_) } @$args);
+    }
 
     $opts->{STYLE} = "debrept";
     $opts->{LAYOUT} =
@@ -58,29 +60,48 @@ sub _perform {
 	{ name  => "amount", title => _T("Bedrag"),       width => 10, align => ">" },
 	{ name  => "open",   title => _T("Openstaand"),   width => 10, align => ">" },
 	{ name  => "paid",   title => _T("Betaald"),      width => 10, align => ">" },
-	{ name  => "bsknr",  title => _T("Boekstuk"),     width => 13 },
+	{ name  => "bsknr",  title => _T("Boekstuk"),     width => 18 },
       ];
 
     my $rep = EB::Report::GenBase->backend($self, { %$opts, debcrd => $debcrd });
 
-    my $sth = $dbh->sql_exec("SELECT DISTINCT bsr_rel_code".
-			     " FROM Boekstukregels, Boekstukken, Dagboeken".
-			     " WHERE bsr_date >= ? AND bsr_date <= ?".
-			     " AND bsr_bsk_id = bsk_id".
-			     " AND bsk_dbk_id = dbk_id".
-			     " AND dbk_type = ?".
-			     ($args ? " AND bsr_rel_code IN (?)" : "").
-			     " ORDER BY bsr_rel_code",
-			     @{$rep->{periode}},
-			     $debcrd ? DBKTYPE_VERKOOP : DBKTYPE_INKOOP,
-			     $args ? $args : ());
+    my %rels;
+    my $sth;
 
-    my @rels;
+    $sth = $dbh->sql_exec("SELECT DISTINCT bsr_rel_code".
+			  " FROM Boekstukregels, Boekstukken, Dagboeken".
+			  " WHERE bsr_date >= ? AND bsr_date <= ?".
+			  " AND bsr_bsk_id = bsk_id".
+			  " AND bsk_dbk_id = dbk_id".
+			  " AND dbk_type = ?",
+			  @{$rep->{periode}},
+			  $debcrd ? DBKTYPE_VERKOOP : DBKTYPE_INKOOP);
     while ( my $rr = $sth->fetchrow_arrayref ) {
-	push(@rels, $rr->[0]);
+	next if $args && $rr->[0] !~ /^$args$/i;
+	$rels{$rr->[0]} = undef;
     }
     $sth->finish;
-    return "!"._T("Geen boekingen gevonden") unless @rels;
+
+    $sth = $dbh->sql_exec("SELECT bsr_rel_code, bsr_paid".
+			  " FROM Boekstukregels, Boekstukken".
+			  " WHERE bsr_date >= ? AND bsr_date <= ?".
+			  " AND bsr_paid = bsk_id".
+			  " AND bsk_date < ?".
+			  " AND bsr_type = ?",
+			  @{$rep->{periode}},
+			  $rep->{per_begin},
+			  $debcrd ? 1 : 2,
+			 );
+
+    while ( my $rr = $sth->fetchrow_arrayref ) {
+	next if $args && $rr->[0] !~ /^$args$/i;
+	$rels{$rr->[0]}->{$rr->[1]} = 1;
+    }
+    $sth->finish;
+    return "!"._T("Geen boekingen gevonden") unless %rels;
+
+    use Data::Dumper;
+    warn(Dumper(\%rels),"\n");
 
     $rep->start($debcrd ? _T("Debiteurenadministratie")
 	                : _T("Crediteurenadministratie"));
@@ -88,31 +109,119 @@ sub _perform {
     my $a_grand = 0;
     my $o_grand = 0;
 
-    foreach my $rel ( @rels ) {
+    foreach my $rel ( sort(keys(%rels)) ) {
 
 	my $a_tot = 0;
 	my $o_tot = 0;
 
-	my $sth = $dbh->sql_exec("SELECT bsk_id, bsk_desc, bsk_date,".
-				 " bsk_amount, bsk_open, dbk_desc, bsk_nr".
-				 " FROM Boekstukken, Boekstukregels, Dagboeken".
-				 " WHERE bsr_date >= ? AND bsr_date <= ?".
-				 " AND bsr_bsk_id = bsk_id".
-				 " AND bsk_dbk_id = dbk_id".
-				 " AND bsr_type = 0".
-				 " AND bsr_nr = 1".
-				 " AND bsr_rel_code = ?".
-				 " ORDER BY bsk_date",
-				 @{$rep->{periode}},
-				 $rel);
-
 	$rep->add({ debcrd => $rel, _style=> "h1" });
+
+	my $sth;
+
+	# Process betalingen zonder boekstuk.
+
+	if ( $rels{$rel} ) {
+	    foreach my $bsk_id ( keys %{$rels{$rel}} ) {
+
+		my $sth;
+		$sth = $dbh->sql_exec("SELECT bsk_id, bsk_desc, bsk_date,".
+				      " bsk_amount, bsk_open, dbk_desc, bsk_nr, bsk_bky".
+				      " FROM Boekstukken, Boekstukregels, Dagboeken".
+				      " WHERE bsk_id = ?".
+				      " AND bsk_dbk_id = dbk_id".
+				      " AND bsr_bsk_id = bsk_id",
+				      $bsk_id);
+
+		my $rr = $sth->fetchrow_arrayref;
+		$sth->finish;
+		my ($bsk_id, $bsk_desc, $bsk_date,
+		    $bsr_amount, $bsr_open, $dbk_desc, $bsk_nr, $bsk_bky) = @$rr;
+
+		# Correct for future payments.
+		my $rop = $dbh->do("SELECT sum(bsr_amount)".
+				   " FROM Boekstukregels".
+				   " WHERE bsr_type = ?".
+				   " AND bsr_date > ?".
+				   " AND bsr_paid = ?",
+				   $debcrd ? 1 : 2, $rep->{per_end}, $bsk_id);
+
+		if ( $rop && $rop->[0] ) {
+		    $bsr_open -= $rop->[0];
+		}
+
+		$bsr_amount = 0-$bsr_amount unless $debcrd;
+		$bsr_open   = 0-$bsr_open   unless $debcrd;
+		$a_tot += $bsr_amount;
+		$o_tot += $bsr_open;
+
+		$rep->add({ desc   => $bsk_desc,
+			    date   => $bsk_date,
+			    amount => numfmt($bsr_amount),
+			    open   => numfmt($bsr_open),
+			    bsknr  => join(":", $dbk_desc, $bsk_bky, $bsk_nr),
+			    _style => "bskprv",
+			  });
+
+		$sth = $dbh->sql_exec("SELECT bsr_date, bsr_desc, bsr_amount,".
+				      " dbk_desc, bsk_nr".
+				      " FROM Boekstukregels, Boekstukken, Dagboeken".
+				      " WHERE bsr_type = ?".
+				      " AND bsr_date >= ? AND bsr_date <= ?".
+				      " AND bsr_paid = ?".
+				      " AND bsr_bsk_id = bsk_id AND bsk_dbk_id = dbk_id".
+				      " ORDER BY bsr_date",
+				      $debcrd ? 1 : 2, @{$rep->{periode}}, $bsk_id);
+		while ( my $rr = $sth->fetchrow_arrayref ) {
+		    my ($x_bsr_date, $x_bsr_desc, $x_bsr_amount,
+			$x_dbk_desc, $x_bsk_nr, $x_bsk_bky) = @$rr;
+		    $x_bsr_amount = 0-$x_bsr_amount unless $debcrd;
+		    $rep->add({ desc    => $x_bsr_desc,
+				date    => $x_bsr_date,
+				paid    => numfmt(0-$x_bsr_amount),
+				bsknr   => join(":", $x_dbk_desc, $x_bsk_nr),
+				_style  => "paid",
+			      });
+		}
+
+
+	    }
+	}
+
+	# Process boekstukken met evt. betalingen.
+
+	$sth = $dbh->sql_exec("SELECT bsk_id, bsk_desc, bsk_date,".
+			      " bsk_amount, bsk_open, dbk_desc, bsk_nr".
+			      " FROM Boekstukken, Boekstukregels, Dagboeken".
+			      " WHERE bsr_date >= ? AND bsr_date <= ?".
+			      " AND bsr_bsk_id = bsk_id".
+			      " AND bsk_dbk_id = dbk_id".
+			      " AND bsr_type = 0".
+			      " AND bsr_nr = 1".
+			      " AND bsr_rel_code = ?".
+			      " ORDER BY bsk_date",
+			      @{$rep->{periode}},
+			      $rel);
 
 	while ( my $rr = $sth->fetchrow_arrayref ) {
 	    my ($bsk_id, $bsk_desc, $bsk_date,
 		$bsr_amount, $bsr_open, $dbk_desc, $bsk_nr) = @$rr;
+
+	    # Correct for future payments.
+	    my $rop = $dbh->do("SELECT sum(bsr_amount)".
+				     " FROM Boekstukregels".
+				     " WHERE bsr_type = ?".
+				     " AND bsr_date > ?".
+				     " AND bsr_paid = ?",
+				     $debcrd ? 1 : 2, $rep->{per_end}, $bsk_id);
+
+	    if ( $rop && $rop->[0] ) {
+		$bsr_open -= $rop->[0];
+	    }
+
 	    $bsr_amount = 0-$bsr_amount unless $debcrd;
-	    $bsr_open = 0-$bsr_open unless $debcrd;
+	    $bsr_open   = 0-$bsr_open   unless $debcrd;
+	    $a_tot += $bsr_amount;
+	    $o_tot += $bsr_open;
 
 	    $rep->add({ desc   => $bsk_desc,
 			date   => $bsk_date,
@@ -121,16 +230,16 @@ sub _perform {
 			bsknr  => join(":", $dbk_desc, $bsk_nr),
 			_style => "bsk",
 		      });
-	    $a_tot += $bsr_amount;
-	    $o_tot += $bsr_open;
+
 	    my $sth = $dbh->sql_exec("SELECT bsr_date, bsr_desc, bsr_amount,".
 				     " dbk_desc, bsk_nr".
 				     " FROM Boekstukregels, Boekstukken, Dagboeken".
 				     " WHERE bsr_type = ?".
+				     " AND bsr_date >= ? AND bsr_date <= ?".
 				     " AND bsr_paid = ?".
 				     " AND bsr_bsk_id = bsk_id AND bsk_dbk_id = dbk_id".
 				     " ORDER BY bsr_date",
-				     $debcrd ? 1 : 2, $bsk_id);
+				     $debcrd ? 1 : 2, @{$rep->{periode}}, $bsk_id);
 	    while ( my $rr = $sth->fetchrow_arrayref ) {
 		my ($x_bsr_date, $x_bsr_desc, $x_bsr_amount,
 		    $x_dbk_desc, $x_bsk_nr) = @$rr;
