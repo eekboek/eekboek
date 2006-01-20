f93e858ba20ce8c56dd1075e8c2fccbe7dba42aa
jv <>
2006-01-20 08:36:58 +0000

    Fix foutief teken van adm_relatie / verkoop boekingen. Nodig voor decode / export.

--- lib/EB/migrate/001000005001000006.sql
+++ lib/EB/migrate/001000005001000006.sql
@@ -1,8 +1,27 @@
--- Migratie EekBoek database van versie 1.0.3 (EB 0.15) naar 1.0.4.
+-- Migratie EekBoek database van versie 1.0.5 (EB 0.20) naar 1.0.6 (0.32).
 
 BEGIN WORK;
 
+-- Nieuw kolom voor boekstuk saldo.
+-- Nodig voor decode / export.
+
 ALTER TABLE Boekstukken ADD COLUMN bsk_saldo int;
+
+-- Fix foutief teken van adm_relatie / verkoop boekingen.
+-- Nodig voor decode / export.
+
+UPDATE Boekstukregels
+SET bsr_amount = -bsr_amount
+WHERE bsr_id IN
+  ( SELECT bsr_id
+    FROM Boekstukregels, Boekstukken, Dagboeken
+    WHERE bsr_date <=
+      ( SELECT bky_end
+        FROM Boekjaren
+	WHERE bky_code = '<<<<' )
+    AND bsr_bsk_id = bsk_id
+    AND bsk_dbk_id = dbk_id
+    AND dbk_type = 2 );
 
 -- Bump version.
 
