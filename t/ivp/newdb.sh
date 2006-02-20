52de416b8bc8c30abb19749207e8ed13e31ac961
jv <>
2006-02-20 21:58:33 +0000

    Use schema.dat.

--- t/ivp/newdb.sh
+++ t/ivp/newdb.sh
@@ -3,7 +3,7 @@
 : ${EBSHELL:=ebshell -X -f ivp.conf}
 
 # Creeer een nieuwe database en vul de database met het schema.
-$EBSHELL --createdb --schema=sample -c || exit 1
+$EBSHELL --createdb --schema=./schema.dat -c || exit 1
 
 # Voeg de relaties toe.
 $EBSHELL --echo < relaties.eb
