81092969d017819c785435d60547891cb1946cbd
jv <>
2006-02-23 11:45:59 +0000

    Bump to 0.43.

--- lib/EB.pm
+++ lib/EB.pm
@@ -1,10 +1,10 @@
 # EB.pm -- 
-# RCS Info        : $Id: EB.pm,v 1.46 2006/02/20 21:58:52 jv Exp $
+# RCS Info        : $Id: EB.pm,v 1.47 2006/02/23 11:45:59 jv Exp $
 # Author          : Johan Vromans
 # Created On      : Fri Sep 16 18:38:45 2005
 # Last Modified By: Johan Vromans
-# Last Modified On: Mon Feb 20 22:53:16 2006
-# Update Count    : 118
+# Last Modified On: Thu Feb 23 12:45:15 2006
+# Update Count    : 119
 # Status          : Unknown, Use with caution!
 
 our $app;
@@ -15,7 +15,7 @@ use strict;
 use base qw(Exporter);
 
 our $VERSION;
-$VERSION = "0.42";
+$VERSION = "0.43";
 
 our @EXPORT;
 our @EXPORT_OK;
