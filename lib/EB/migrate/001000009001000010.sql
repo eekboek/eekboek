b9a0dab661a26f0434deb43e95bfc9482573e62c
jv <>
2006-09-25 15:16:12 +0000

    Migrate 1.0.9 to 1.0.10.
    - Dagboeken: Add column dbk_dcsplit
      Set to F for all existing entries
    - Journal: Add column jnl_damount

---
+++ lib/EB/migrate/001000009001000010.sql
@@ -0,0 +1,21 @@
+-- Migratie EekBoek database van versie 1.0.9 naar 1.0.10 (EB 1.01.xx).
+
+BEGIN WORK;
+
+-- Table Dagboeken
+ALTER TABLE ONLY Dagboeken ADD COLUMN dbk_dcsplit BOOLEAN DEFAULT false;
+UPDATE Dagboeken SET dbk_dcsplit = 'false';
+
+-- Table Journal
+ALTER TABLE ONLY Journal ADD COLUMN jnl_damount int8;
+
+-- Bump version.
+
+UPDATE Constants
+  SET value = 10
+  WHERE name = 'SCM_REVISION';
+UPDATE Metadata
+  SET adm_scm_revision =
+	(SELECT int4(value) FROM Constants WHERE name = 'SCM_REVISION');
+
+COMMIT WORK;
