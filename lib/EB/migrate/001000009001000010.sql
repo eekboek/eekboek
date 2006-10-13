5d5f9e40ab85207275ed1f3b5aa81872b4dd3f68
jv <>
2006-10-13 13:42:50 +0000

    Omdat bsk_id vanuit diverse andere tabellen wordt gerefereerd als foreign key moeten deze constraints eerst worden vervijderd, en later weer aangemaakt.

--- lib/EB/migrate/001000009001000010.sql
+++ lib/EB/migrate/001000009001000010.sql
@@ -13,12 +13,42 @@ ALTER TABLE ONLY Journal ADD COLUMN jnl_damount int8;
 ALTER TABLE ONLY Boekstukregels DROP COLUMN bsr_id;
 
 -- Table Boekstukken
+
+-- Operatie: wijzig type van bsk_id van serial naar int.
+-- Omdat bsk_id vanuit diverse andere tabellen wordt gerefereerd als
+-- foreign key moeten deze constraints eerst worden vervijderd, en
+-- later weer aangemaakt.
+
+ALTER TABLE ONLY Journal
+    DROP CONSTRAINT "journal_jnl_bsk_id_fkey";
+ALTER TABLE ONLY Boekstukregels
+    DROP CONSTRAINT "boekstukregels_bsr_paid_fkey";
+ALTER TABLE ONLY Boekstukregels
+    DROP CONSTRAINT "boekstukregels_bsr_bsk_id_fkey";
+
+-- De eigenlijke wijziging.
 ALTER TABLE ONLY Boekstukken ADD COLUMN temp int8;
 UPDATE Boekstukken SET temp = bsk_id;
 ALTER TABLE ONLY Boekstukken DROP COLUMN bsk_id;
 ALTER TABLE ONLY Boekstukken RENAME COLUMN temp TO bsk_id;
+ALTER TABLE ONLY Boekstukken
+    ADD CONSTRAINT "boekstukken_pkey"
+    PRIMARY KEY ( bsk_id );
+
+-- Expliciete sequence.
 CREATE SEQUENCE boekstukken_bsk_id_seq;
 SELECT setval('boekstukken_bsk_id_seq', max(bsk_id)) FROM Boekstukken;
+
+-- Restore de FK constraints.
+ALTER TABLE ONLY Journal
+    ADD CONSTRAINT "journal_jnl_bsk_id_fkey"
+    FOREIGN KEY ( jnl_bsk_id ) REFERENCES Boekstukken ( bsk_id );
+ALTER TABLE ONLY Boekstukregels
+    ADD CONSTRAINT "boekstukregels_bsr_paid_fkey"
+    FOREIGN KEY ( bsr_paid ) REFERENCES Boekstukken ( bsk_id );
+ALTER TABLE ONLY Boekstukregels
+    ADD CONSTRAINT "boekstukregels_bsr_bsk_id_fkey"
+    FOREIGN KEY ( bsr_bsk_id ) REFERENCES Boekstukken ( bsk_id );
 
 -- Bump version.
 
