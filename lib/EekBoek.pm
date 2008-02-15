0395fadab4e0f9f61fe8e773847a7e82f34116e0
jv <>
2008-02-15 20:30:47 +0000

    Bump.

--- lib/EekBoek.pm
+++ lib/EekBoek.pm
@@ -6,9 +6,9 @@ package EekBoek;
 
 use strict;
 
-my $RCS_Id = '$Id: EekBoek.pm,v 1.56 2008/02/07 12:03:21 jv Exp $ ';
+my $RCS_Id = '$Id: EekBoek.pm,v 1.57 2008/02/15 20:30:47 jv Exp $ ';
 our $PACKAGE = 'EekBoek';
-our $VERSION = '1.03.08';
+our $VERSION = '1.03.09';
 
 =head1 NAME
 
@@ -59,4 +59,4 @@ GNU General Public License or the Artistic License for more details.
 
 1;
 
-# $Id: EekBoek.pm,v 1.56 2008/02/07 12:03:21 jv Exp $ 
+# $Id: EekBoek.pm,v 1.57 2008/02/15 20:30:47 jv Exp $ 
