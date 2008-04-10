9864cf375638f49c03161826817c6239383bd4cc
jv <>
2008-04-10 12:11:03 +0000

    Bump.

--- lib/EekBoek.pm
+++ lib/EekBoek.pm
@@ -9,9 +9,9 @@ use strict;
 # Minimal version to prevent many Unicode bugs.
 use 5.8.3;
 
-my $RCS_Id = '$Id: EekBoek.pm,v 1.64 2008/04/09 21:00:41 jv Exp $ ';
+my $RCS_Id = '$Id: EekBoek.pm,v 1.65 2008/04/10 12:11:03 jv Exp $ ';
 our $PACKAGE = 'EekBoek';
-our $VERSION = '1.03.12';
+our $VERSION = '1.03.13';
 
 =head1 NAME
 
@@ -62,4 +62,4 @@ GNU General Public License or the Artistic License for more details.
 
 1;
 
-# $Id: EekBoek.pm,v 1.64 2008/04/09 21:00:41 jv Exp $ 
+# $Id: EekBoek.pm,v 1.65 2008/04/10 12:11:03 jv Exp $ 
