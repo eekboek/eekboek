be169d2b96cbff2208480ccd9cb1add6fcd2933e
jv <>
2009-10-09 15:44:35 +0000

    Accounts: dcfixed.

---
+++ lib/EB/migrate/001000013001000014.sql
@@ -0,0 +1,17 @@
+BEGIN WORK;
+
+ALTER TABLE Accounts
+ADD COLUMN acc_dcfixed      boolean;	-- fixed d/c
+
+UPDATE Accounts SET acc_dcfixed = 'f' WHERE acc_balres;
+
+-- Bump version.
+
+UPDATE Constants
+  SET value = '14'
+  WHERE name = 'SCM_REVISION' AND value = '13';
+UPDATE Metadata
+  SET adm_scm_revision =
+    (SELECT int2(value) FROM Constants WHERE name = 'SCM_REVISION');
+
+COMMIT WORK;
