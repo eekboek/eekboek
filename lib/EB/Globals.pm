0bd910a2fba59ebb380895b4cc0f37b05aaa6255
jv <>
2009-10-24 15:08:25 +0000

    Eliminate BEGIN blocks. Refactor, make more elegant.

--- lib/EB/Globals.pm
+++ lib/EB/Globals.pm
@@ -7,12 +7,13 @@ package EB::Globals;
 use strict;
 use warnings;
 
-our $VERSION = sprintf "%d.%03d", q$Revision: 1.32 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%03d", q$Revision: 1.33 $ =~ /(\d+)/g;
 
 use base qw(Exporter);
 
 our @EXPORT;
 
+# Define new constant subroutine, and add it to @EXPORT.
 sub _newconst($$) {
     my $t = $_[1];
     $t = "'$t'" unless $t =~ /^\d+$/ || $t =~ /^\[.*\]$/;
@@ -21,55 +22,60 @@ sub _newconst($$) {
     push(@EXPORT, $_[0]);
 }
 
+# Define an enumeration of constant subroutines.
+sub _enumconst($@) {
+    my ($pfx, @list) = @_;
+    my $index = 0;
+    foreach ( @list ) {
+	my $key = $pfx.$_;
+	if ( $key =~ /^(.*)=(\d+)$/ ) {
+	    $key = $1;
+	    $index = $2;
+	}
+	_newconst( $key, $index++ );
+    }
+}
+
+# To defeat gettext. Strings here are not for translation.
 sub N__($) { $_[0] }
 
-BEGIN {
-    _newconst("SCM_MAJVERSION", 1);
-    _newconst("SCM_MINVERSION", 0);
-    _newconst("SCM_REVISION",  14);
+_newconst("SCM_MAJVERSION",  1);
+_newconst("SCM_MINVERSION",  0);
+_newconst("SCM_REVISION",   14);
 
-    _newconst("AMTPRECISION",   2);
-    _newconst("AMTWIDTH",       9);
-    _newconst("BTWPRECISION",   4);
-    _newconst("BTWWIDTH",       5);
-    _newconst("AMTSCALE",     100);
-    _newconst("BTWSCALE",   10000);
+_newconst("AMTPRECISION",    2);
+_newconst("AMTWIDTH",        9);
+_newconst("BTWPRECISION",    4);
+_newconst("BTWWIDTH",        5);
+_newconst("AMTSCALE",      100);
+_newconst("BTWSCALE",    10000);
 
-    _newconst("BKY_PREVIOUS", "<<<<");
+_newconst("BKY_PREVIOUS", "<<<<");
 
-    my $i = 1;
-    map { _newconst("DBKTYPE_$_", $i++) }
-      qw(INKOOP VERKOOP BANK KAS MEMORIAAL);
-    _newconst("DBKTYPES",
-	      "[qw(".N__("-- Inkoop Verkoop Bank Kas Memoriaal").")]");
-    $i = 0;
-    map { _newconst("BTWTARIEF_$_", $i++) }
-      qw(NUL HOOG LAAG PRIV ANDERS);
-    _newconst("BTWTARIEVEN", "[qw(".N__("Nul Hoog Laag Privé Anders").")]");
-    _newconst("BTWPERIODES", "[qw(".N__("Geen Jaar 2 3 Kwartaal 5 6 7 8 9 10 11 Maand").")]");
-    _newconst("BTWPER_GEEN", 0);
-    _newconst("BTWPER_JAAR", 1);
-    _newconst("BTWPER_KWARTAAL", 4);
-    _newconst("BTWPER_MAAND", 12);
+_enumconst("DBKTYPE_", qw(INKOOP=1 VERKOOP BANK KAS MEMORIAAL) );
+_newconst("DBKTYPES",
+	  "[qw(".N__("-- Inkoop Verkoop Bank Kas Memoriaal").")]");
 
-    $i = 0;
-    map { _newconst("BTWTYPE_$_", $i++) }
-      qw(NORMAAL VERLEGD INTRA EXTRA);
-    _newconst("BTWTYPES", "[qw(".N__("Normaal Verlegd Intra Extra").")]");
+_enumconst("BTWTARIEF_", qw(NUL=0 HOOG LAAG PRIV ANDERS) );
+_newconst("BTWTARIEVEN", "[qw(".N__("Nul Hoog Laag Privé Anders").")]");
+_newconst("BTWPERIODES", "[qw(".N__("Geen Jaar 2 3 Kwartaal 5 6 7 8 9 10 11 Maand").")]");
+_newconst("BTWPER_GEEN", 0);
+_newconst("BTWPER_JAAR", 1);
+_newconst("BTWPER_KWARTAAL", 4);
+_newconst("BTWPER_MAAND", 12);
+_enumconst("BTWTYPE_", qw(NORMAAL=0 VERLEGD INTRA EXTRA) );
+_newconst("BTWTYPES", "[qw(".N__("Normaal Verlegd Intra Extra").")]");
+_newconst("BTWKLASSE_BTW_BIT",   0x200);
+_newconst("BTWKLASSE_KO_BIT",    0x100);
+_newconst("BTWKLASSE_TYPE_BITS", 0x0ff);
 
-    _newconst("BTWKLASSE_BTW_BIT",   0x200);
-    _newconst("BTWKLASSE_KO_BIT",    0x100);
-    _newconst("BTWKLASSE_TYPE_BITS", 0x0ff);
-
-}
-
-sub BTWKLASSE($$$) {
-    ($_[0] ? BTWKLASSE_BTW_BIT : 0)
-      | ($_[1] ? ($_[1] & BTWKLASSE_TYPE_BITS) : 0)
-	| ($_[2] ? BTWKLASSE_KO_BIT : 0);
-}
-
-BEGIN { push(@EXPORT, qw(BTWKLASSE)) }
+# Eval, since it uses the (run-time defined) subroutines.
+eval( 'sub BTWKLASSE($$$) {'.
+      ' ($_[0] ? BTWKLASSE_BTW_BIT : 0)'.
+      ' | ($_[1] ? ($_[1] & BTWKLASSE_TYPE_BITS) : 0)'.
+      ' | ($_[2] ? BTWKLASSE_KO_BIT : 0);'.
+      '}' );
+push(@EXPORT, qw(BTWKLASSE));
 
 unless ( caller ) {
     print STDOUT ("-- Constants\n\n",
