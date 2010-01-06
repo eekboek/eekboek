f375211e881cd3279f9dc048d263fe13d6211bf2
jv <>
2010-01-06 20:17:43 +0000

    Bump.

--- lib/EekBoek.pm
+++ lib/EekBoek.pm
@@ -10,9 +10,9 @@ use utf8;
 # Minimal version to prevent many Unicode bugs.
 use 5.008003;
 
-my $RCS_Id = '$Id: EekBoek.pm,v 1.87 2010/01/06 19:04:16 jv Exp $ ';
+my $RCS_Id = '$Id: EekBoek.pm,v 1.88 2010/01/06 20:17:43 jv Exp $ ';
 our $PACKAGE = 'EekBoek';
-our $VERSION = '1.05.12';
+our $VERSION = '1.05.13';
 
 =head1 NAME
 
@@ -63,4 +63,4 @@ GNU General Public License or the Artistic License for more details.
 
 1;
 
-# $Id: EekBoek.pm,v 1.87 2010/01/06 19:04:16 jv Exp $ 
+# $Id: EekBoek.pm,v 1.88 2010/01/06 20:17:43 jv Exp $ 
