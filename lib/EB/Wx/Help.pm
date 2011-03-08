a1367479452ca5b2be112ce01a2c09b67ae9235a
Johan Vromans <jvromans@squirrel.nl>
2011-03-08 13:47:28 +0100

    (Wx/Help) Try to set accellerators (but still doesn't work.)

--- lib/EB/Wx/Help.pm
+++ lib/EB/Wx/Help.pm
@@ -9,9 +9,9 @@ package EB::Wx::Help;
 use strict;
 use EB;
 
-use Wx qw(wxHF_FLATTOOLBAR wxHF_DEFAULTSTYLE);
+use Wx qw(wxHF_FLATTOOLBAR wxHF_TOOLBAR wxHF_CONTENTS wxHF_SEARCH wxHF_BOOKMARKS wxHF_INDEX wxHF_PRINT wxHF_DEFAULTSTYLE);
 use Wx qw(wxACCEL_CTRL wxACCEL_NORMAL wxID_CLOSE);
-use Wx::Event qw(EVT_BUTTON);
+use Wx::Event;
 use Wx::Html;
 use Wx::Help;
 use Wx::FS;
@@ -21,17 +21,15 @@ Wx::FileSystem::AddHandler( new Wx::ZipFSHandler );
 
 sub new {
     my $class = shift;
-    my $self = Wx::HtmlHelpController->new( wxHF_FLATTOOLBAR|wxHF_DEFAULTSTYLE );
-
-    $self->GetHelpWindow;
-    # For convenience: CLOSE on Ctrl-W and Esc.
-    # (Doesn't work on GTK, yet).
-#    $self->SetAcceleratorTable
- #     (Wx::AcceleratorTable->new
-  #     ( [wxACCEL_CTRL, ord 'w', wxID_CLOSE],
-#	 [wxACCEL_NORMAL, 27, wxID_CLOSE],
- #      ));
-
+    my $self = Wx::HtmlHelpController->new
+      ( wxHF_FLATTOOLBAR | wxHF_TOOLBAR
+#	| wxHF_CONTENTS
+#	| wxHF_INDEX
+	| wxHF_CONTENTS
+#	| wxHF_BOOKMARKS
+	| wxHF_SEARCH
+	| wxHF_PRINT
+      );
     return bless \$self, $class;
 }
 
@@ -41,11 +39,22 @@ sub show_html_help {
     if ( my $htb_file = findlib( "docs.htb", "help" ) ) {
 	$$self->AddBook( $htb_file, 1 );
 	$$self->DisplayContents;
+	if( my $hframe = Wx::Window::FindWindowByName('wxHtmlHelp')) {
+	    $hframe->SetAcceleratorTable
+	      (Wx::AcceleratorTable->new
+	       ( [wxACCEL_CTRL, ord 'w', wxID_CLOSE],
+		 [wxACCEL_NORMAL, 27, wxID_CLOSE],
+	       ));
+	}
     }
     else {
 	::info( _T("No help available for this language"),
 	        _T("Sorry") );
     }
 }
+
+package Wx::HtmlHelpFrame;
+
+our @ISA = qw( Wx::Frame );
 
 1;
