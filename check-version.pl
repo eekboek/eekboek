a04e4732a7a6e1c65d6f4fe2b135e0919b105452
Johan Vromans <jvromans@squirrel.nl>
2020-09-18 11:39:23 +0200

    (check-version) Add lib to @INC.

--- check-version.pl
+++ check-version.pl
@@ -3,7 +3,8 @@
 use strict;
 use warnings;
 
-use lib::EB::Version;
+use lib 'lib';
+use EB::Version;
 
 $SIG{INT} = sub { die("No\n") };
 print STDERR ("Version = $EB::Version::VERSION -- Continue? ");
